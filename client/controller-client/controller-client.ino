3b257dc84828ce8ff84e7f44650e3a3160f409f0
Samuel Hindmarsh <samuel.hindmarsh@trademe.co.nz>
2016-02-04 18:45:14 +1300

    Controller client should be finished, still working on server

--- client/controller-client/controller-client.ino
+++ client/controller-client/controller-client.ino
@@ -1,7 +1,81 @@
-const int PIN_1_START = 10;
-const int PIN_1_COIN = 11;
+/*
+ * Payload description:
+ *   4 bytes per packet; described from MSB to LSB
+ * 
+ * Byte0:
+ *   P1_UP
+ *   P1_DOWN
+ *   P1_LEFT
+ *   P1_RIGHT
+ *   P2_UP
+ *   P2_DOWN
+ *   P2_LEFT
+ *   P2_RIGHT
+ *
+ * Byte1:
+ *   P1_COIN
+ *   P1_START
+ *   P1_B1
+ *   P1_B2
+ *   P1_B3
+ *   P1_B4
+ *   P1_B5
+ *   P1_B6
+ *
+ * Byte2:
+ *   P1_B7
+ *   P2_COIN
+ *   P2_START
+ *   P2_B1
+ *   P2_B2
+ *   P2_B3
+ *   P2_B4
+ *   P2_B5
+ *
+ * Byte3:
+ *   P2_B6
+ *   P2_B7
+ *   NONE
+ *   NONE
+ *   NONE
+ *   NONE
+ *   NONE
+ *   NONE
+ */
 
-byte btns;
+const int PAYLOAD_SIZE = 4;
+
+const int PIN_P1_UP = 22
+const int PIN_P1_DOWN = 24
+const int PIN_P1_LEFT = 26
+const int PIN_P1_RIGHT = 28
+const int PIN_P2_UP = 23
+const int PIN_P2_DOWN = 25
+const int PIN_P2_LEFT = 27
+const int PIN_P2_RIGHT = 29
+
+const int PIN_P1_COIN = 30
+const int PIN_P1_START = 32
+const int PIN_P1_B1 = 34
+const int PIN_P1_B2 = 36
+const int PIN_P1_B3 = 38
+const int PIN_P1_B4 = 40
+const int PIN_P1_B5 = 42
+const int PIN_P1_B6 = 44
+
+const int PIN_P1_B7 = 46
+const int PIN_P2_COIN = 31
+const int PIN_P2_START = 33
+const int PIN_P2_B1 = 35
+const int PIN_P2_B2 = 37
+const int PIN_P2_B3 = 39
+const int PIN_P2_B4 = 41
+const int PIN_P2_B5 = 43
+
+const int PIN_P2_B6 = 45
+const int PIN_P2_B7 = 47
+
+byte btns[PAYLOAD_SIZE];
 
 void setup() {
   // start serial port at 9600 bps:
@@ -10,26 +84,74 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  pinMode(PIN_1_START, INPUT);
-  pinMode(PIN_1_COIN, INPUT);
-
-  pinMode(13, OUTPUT); // debugging
-  
+  pinMode(PIN_P1_UP, INPUT);
+  pinMode(PIN_P1_DOWN, INPUT);
+  pinMode(PIN_P1_LEFT, INPUT);
+  pinMode(PIN_P1_RIGHT, INPUT);
+  pinMode(PIN_P2_UP, INPUT);
+  pinMode(PIN_P2_DOWN, INPUT);
+  pinMode(PIN_P2_LEFT, INPUT);
+  pinMode(PIN_P2_RIGHT, INPUT);
+  pinMode(PIN_P1_COIN, INPUT);
+  pinMode(PIN_P1_START, INPUT);
+  pinMode(PIN_P1_B1, INPUT);
+  pinMode(PIN_P1_B2, INPUT);
+  pinMode(PIN_P1_B3, INPUT);
+  pinMode(PIN_P1_B4, INPUT);
+  pinMode(PIN_P1_B5, INPUT);
+  pinMode(PIN_P1_B6, INPUT);
+  pinMode(PIN_P1_B7, INPUT);
+  pinMode(PIN_P2_COIN, INPUT);
+  pinMode(PIN_P2_START, INPUT);
+  pinMode(PIN_P2_B1, INPUT);
+  pinMode(PIN_P2_B2, INPUT);
+  pinMode(PIN_P2_B3, INPUT);
+  pinMode(PIN_P2_B4, INPUT);
+  pinMode(PIN_P2_B5, INPUT);
+  pinMode(PIN_P2_B6, INPUT);
+  pinMode(PIN_P2_B7, INPUT);
 }
 
 
 void loop() {
-  if(Serial) {
-    btns = 0;
-    
-    if(digitalRead(PIN_1_START) == HIGH) {
-      btns |= (1 << 7);
-    }
+  if(Serial) {    
+    for(int b = 0; b < PAYLOAD_SIZE; b++) {
+  	  btns[b] = 0;
+  	}
+      
+    btns[0] |= (buttonState(PIN_P1_UP) << 7);
+    btns[0] |= (buttonState(PIN_P1_DOWN) << 6);
+    btns[0] |= (buttonState(PIN_P1_LEFT) << 5);
+    btns[0] |= (buttonState(PIN_P1_RIGHT) << 4);
+    btns[0] |= (buttonState(PIN_P2_UP) << 3);
+    btns[0] |= (buttonState(PIN_P2_DOWN) << 2);
+    btns[0] |= (buttonState(PIN_P2_LEFT) << 1);
+    btns[0] |= (buttonState(PIN_P2_RIGHT));
+    btns[1] |= (buttonState(PIN_P1_COIN) << 7);
+    btns[1] |= (buttonState(PIN_P1_START) << 6);
+    btns[1] |= (buttonState(PIN_P1_B1) << 5);
+    btns[1] |= (buttonState(PIN_P1_B2) << 4);
+    btns[1] |= (buttonState(PIN_P1_B3) << 3);
+    btns[1] |= (buttonState(PIN_P1_B4) << 2);
+    btns[1] |= (buttonState(PIN_P1_B5) << 1);
+    btns[1] |= (buttonState(PIN_P1_B6));
+    btns[2] |= (buttonState(PIN_P1_B7) << 7);
+    btns[2] |= (buttonState(PIN_P2_COIN) << 6);
+    btns[2] |= (buttonState(PIN_P2_START) << 5);
+    btns[2] |= (buttonState(PIN_P2_B1) << 4);
+    btns[2] |= (buttonState(PIN_P2_B2) << 3);
+    btns[2] |= (buttonState(PIN_P2_B3) << 2);
+    btns[2] |= (buttonState(PIN_P2_B4) << 1);
+    btns[2] |= (buttonState(PIN_P2_B5));
+    btns[3] |= (buttonState(PIN_P2_B6) << 7);
+    btns[3] |= (buttonState(PIN_P2_B7) << 6);
 
-    if(digitalRead(PIN_1_COIN) == HIGH) {
-      btns |= (1 << 6);
-    }
+    Serial.write(btns, PAYLOAD_SIZE);
+  }
+}
 
-    Serial.write(btns);
+int buttonState(int pin) {
+  if(digitalRead(pin) == HIGH) {
+    return 1;
   }
 }
--- server/server.py
+++ server/server.py
@@ -1,18 +1,124 @@
+# See controller-client.ino for a description of the serial payload.
+#
+# Button mapping:
+#
+# P1_UP = up
+# P1_DOWN = down
+# P1_LEFT = left
+# P1_RIGHT = right
+# P1_COIN = 5
+# P1_START = 1
+# P1_B1 = L
+# P1_B2 = ;
+# P1_B3 = '
+# P1_B4 = ,
+# P1_B5 = .
+# P1_B6 = /
+# P1_B7 = M
+#
+# P2_UP = W
+# P2_DOWN = S
+# P2_LEFT = A
+# P2_RIGHT = D
+# P2_COIN = 6
+# P2_START = 2
+# P2_B1 = T
+# P2_B2 = Y
+# P2_B3 = U
+# P2_B4 = G
+# P2_B5 = H
+# P2_B6 = J
+# P2_B7 = F
+
 import serial
 import time
 import win32api
 import win32con
 
-f = 0x46 # VirtualKey Code of the letter "F", see http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx 
-g = 0x47 # VirtualKey Code of the letter "F", see http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx 
+PAYLOAD_SIZE = 4
+
+VK_LEFT = 0x25
+VK_UP = 0x26
+VK_RIGHT = 0x27
+VK_DOWN = 0x28
+VK_1 = 0x31
+VK_2 = 0x32
+VK_5 = 0x35
+VK_6 = 0x36
+VK_A = 0x41
+VK_D = 0x44
+VK_F = 0x46
+VK_G = 0x47
+VK_H = 0x48
+VK_J = 0x4A
+VK_L = 0x4C
+VK_M = 0x4D
+VK_S = 0x53
+VK_T = 0x54
+VK_U = 0x55
+VK_W = 0x57
+VK_Y = 0x59
+VK_SEMICOLON = 0xBA
+VK_COMMA = 0XBC
+VK_FULLSTOP = 0XBE
+VK_SLASH = 0xBF
+VK_QUOTE = 0xDE
 
 ss = serial.Serial('COM3', 9600)
-time.sleep(1)
+time.sleep(1) # wait 1 second to ensure connection
 
-while 1:
+cached_btns = bytearray([0,0,0,0])
+btns = bytearray([0,0,0,0])
+
+def update_button_state( byte_num, mask, vkey ):
+	if cached_btns[byte_num] & mask > btns[byte_num] & mask:
+		# button released, send KEYUP event
+		win32api.keybd_event(vkey, 0, win32con.KEYEVENTF_KEYUP, 0)
+	else if btns[byte_num] & mask > cached_btns[byte_num] & mask:
+		# button pressed, send KEYDOWN event
+		win32api.keybd_event(vkey, 0, 0, 0)
+
+while True:
 	try:
-		val = ss.read()
-		print('{0:08b}'.format(ord(val)))
+		cached_btns[:] = btns[:]; # cache buttons state
+
+		# load button states
+		btns = bytearray([0,0,0,0])
+		for b in range(0,PAYLOAD_SIZE):
+			btns[b] = ss.read()
+			print('{0:08b}'.format(ord(val)) + ' ') # debugging: print binary representation of bytes
+		print('\n') # debugging: newline
+
+		# byte0
+		update_button_state(0, 0b10000000, VK_UP)
+		update_button_state(0, 0b01000000, VK_DOWN)
+		update_button_state(0, 0b00100000, VK_LEFT)
+		update_button_state(0, 0b00010000, VK_RIGHT)
+		update_button_state(0, 0b00001000, VK_W)
+		update_button_state(0, 0b00000100, VK_S)
+		update_button_state(0, 0b00000010, VK_A)
+		update_button_state(0, 0b00000001, VK_D)
+		# byte1
+		update_button_state(0, 0b10000000, VK_5)
+		update_button_state(0, 0b01000000, VK_1)
+		update_button_state(0, 0b00100000, VK_L)
+		update_button_state(0, 0b00010000, VK_SEMICOLON)
+		update_button_state(0, 0b00001000, VK_QUOTE)
+		update_button_state(0, 0b00000100, VK_S)
+		update_button_state(0, 0b00000010, VK_A)
+		update_button_state(0, 0b00000001, VK_D)
+		# byte2
+		update_button_state(0, 0b10000000, VK_UP)
+		update_button_state(0, 0b01000000, VK_DOWN)
+		update_button_state(0, 0b00100000, VK_LEFT)
+		update_button_state(0, 0b00010000, VK_RIGHT)
+		update_button_state(0, 0b00001000, VK_W)
+		update_button_state(0, 0b00000100, VK_S)
+		update_button_state(0, 0b00000010, VK_A)
+		update_button_state(0, 0b00000001, VK_D)
+		# byte3
+		update_button_state(0, 0b10000000, VK_UP)
+		update_button_state(0, 0b01000000, VK_DOWN)
 
 		if ord(val) & 0b10000000 > 0:
 			win32api.keybd_event(f,0,0,0)
@@ -21,5 +127,6 @@ while 1:
 			win32api.keybd_event(g,0,0,0)
 			win32api.keybd_event(g,0,win32con.KEYEVENTF_KEYUP,0)
 
-	except ss.SerialTimeoutException:
-		print('Data could not be read')
+	except serial.serialutil.SerialException:
+		release_keys()
+		ss.close()
