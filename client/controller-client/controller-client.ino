cc807afe5578f5c46c9f0e42873509a3c33f21ae
Samuel Hindmarsh <Samuel.Hindmarsh@gmail.com>
2016-02-05 00:19:58 +1300

    Client working, server works for player one's movement. Need to change all keycodes to scancodes

--- client/controller-client/controller-client.ino
+++ client/controller-client/controller-client.ino
@@ -45,35 +45,35 @@
 
 const int PAYLOAD_SIZE = 4;
 
-const int PIN_P1_UP = 22
-const int PIN_P1_DOWN = 24
-const int PIN_P1_LEFT = 26
-const int PIN_P1_RIGHT = 28
-const int PIN_P2_UP = 23
-const int PIN_P2_DOWN = 25
-const int PIN_P2_LEFT = 27
-const int PIN_P2_RIGHT = 29
+const int PIN_P1_UP = 22;
+const int PIN_P1_DOWN = 24;
+const int PIN_P1_LEFT = 26;
+const int PIN_P1_RIGHT = 28;
+const int PIN_P2_UP = 23;
+const int PIN_P2_DOWN = 25;
+const int PIN_P2_LEFT = 27;
+const int PIN_P2_RIGHT = 29;
 
-const int PIN_P1_COIN = 30
-const int PIN_P1_START = 32
-const int PIN_P1_B1 = 34
-const int PIN_P1_B2 = 36
-const int PIN_P1_B3 = 38
-const int PIN_P1_B4 = 40
-const int PIN_P1_B5 = 42
-const int PIN_P1_B6 = 44
+const int PIN_P1_COIN = 30;
+const int PIN_P1_START = 32;
+const int PIN_P1_B1 = 34;
+const int PIN_P1_B2 = 36;
+const int PIN_P1_B3 = 38;
+const int PIN_P1_B4 = 40;
+const int PIN_P1_B5 = 42;
+const int PIN_P1_B6 = 44;
 
-const int PIN_P1_B7 = 46
-const int PIN_P2_COIN = 31
-const int PIN_P2_START = 33
-const int PIN_P2_B1 = 35
-const int PIN_P2_B2 = 37
-const int PIN_P2_B3 = 39
-const int PIN_P2_B4 = 41
-const int PIN_P2_B5 = 43
+const int PIN_P1_B7 = 46;
+const int PIN_P2_COIN = 31;
+const int PIN_P2_START = 33;
+const int PIN_P2_B1 = 35;
+const int PIN_P2_B2 = 37;
+const int PIN_P2_B3 = 39;
+const int PIN_P2_B4 = 41;
+const int PIN_P2_B5 = 43;
 
-const int PIN_P2_B6 = 45
-const int PIN_P2_B7 = 47
+const int PIN_P2_B6 = 45;
+const int PIN_P2_B7 = 47;
 
 byte btns[PAYLOAD_SIZE];
 
@@ -88,28 +88,28 @@ void setup() {
   pinMode(PIN_P1_DOWN, INPUT);
   pinMode(PIN_P1_LEFT, INPUT);
   pinMode(PIN_P1_RIGHT, INPUT);
-  pinMode(PIN_P2_UP, INPUT);
-  pinMode(PIN_P2_DOWN, INPUT);
-  pinMode(PIN_P2_LEFT, INPUT);
-  pinMode(PIN_P2_RIGHT, INPUT);
-  pinMode(PIN_P1_COIN, INPUT);
-  pinMode(PIN_P1_START, INPUT);
-  pinMode(PIN_P1_B1, INPUT);
-  pinMode(PIN_P1_B2, INPUT);
-  pinMode(PIN_P1_B3, INPUT);
-  pinMode(PIN_P1_B4, INPUT);
-  pinMode(PIN_P1_B5, INPUT);
-  pinMode(PIN_P1_B6, INPUT);
-  pinMode(PIN_P1_B7, INPUT);
-  pinMode(PIN_P2_COIN, INPUT);
-  pinMode(PIN_P2_START, INPUT);
-  pinMode(PIN_P2_B1, INPUT);
-  pinMode(PIN_P2_B2, INPUT);
-  pinMode(PIN_P2_B3, INPUT);
-  pinMode(PIN_P2_B4, INPUT);
-  pinMode(PIN_P2_B5, INPUT);
-  pinMode(PIN_P2_B6, INPUT);
-  pinMode(PIN_P2_B7, INPUT);
+//  pinMode(PIN_P2_UP, INPUT);
+//  pinMode(PIN_P2_DOWN, INPUT);
+//  pinMode(PIN_P2_LEFT, INPUT);
+//  pinMode(PIN_P2_RIGHT, INPUT);
+//  pinMode(PIN_P1_COIN, INPUT);
+//  pinMode(PIN_P1_START, INPUT);
+//  pinMode(PIN_P1_B1, INPUT);
+//  pinMode(PIN_P1_B2, INPUT);
+//  pinMode(PIN_P1_B3, INPUT);
+//  pinMode(PIN_P1_B4, INPUT);
+//  pinMode(PIN_P1_B5, INPUT);
+//  pinMode(PIN_P1_B6, INPUT);
+//  pinMode(PIN_P1_B7, INPUT);
+//  pinMode(PIN_P2_COIN, INPUT);
+//  pinMode(PIN_P2_START, INPUT);
+//  pinMode(PIN_P2_B1, INPUT);
+//  pinMode(PIN_P2_B2, INPUT);
+//  pinMode(PIN_P2_B3, INPUT);
+//  pinMode(PIN_P2_B4, INPUT);
+//  pinMode(PIN_P2_B5, INPUT);
+//  pinMode(PIN_P2_B6, INPUT);
+//  pinMode(PIN_P2_B7, INPUT);
 }
 
 
@@ -123,28 +123,28 @@ void loop() {
     btns[0] |= (buttonState(PIN_P1_DOWN) << 6);
     btns[0] |= (buttonState(PIN_P1_LEFT) << 5);
     btns[0] |= (buttonState(PIN_P1_RIGHT) << 4);
-    btns[0] |= (buttonState(PIN_P2_UP) << 3);
-    btns[0] |= (buttonState(PIN_P2_DOWN) << 2);
-    btns[0] |= (buttonState(PIN_P2_LEFT) << 1);
-    btns[0] |= (buttonState(PIN_P2_RIGHT));
-    btns[1] |= (buttonState(PIN_P1_COIN) << 7);
-    btns[1] |= (buttonState(PIN_P1_START) << 6);
-    btns[1] |= (buttonState(PIN_P1_B1) << 5);
-    btns[1] |= (buttonState(PIN_P1_B2) << 4);
-    btns[1] |= (buttonState(PIN_P1_B3) << 3);
-    btns[1] |= (buttonState(PIN_P1_B4) << 2);
-    btns[1] |= (buttonState(PIN_P1_B5) << 1);
-    btns[1] |= (buttonState(PIN_P1_B6));
-    btns[2] |= (buttonState(PIN_P1_B7) << 7);
-    btns[2] |= (buttonState(PIN_P2_COIN) << 6);
-    btns[2] |= (buttonState(PIN_P2_START) << 5);
-    btns[2] |= (buttonState(PIN_P2_B1) << 4);
-    btns[2] |= (buttonState(PIN_P2_B2) << 3);
-    btns[2] |= (buttonState(PIN_P2_B3) << 2);
-    btns[2] |= (buttonState(PIN_P2_B4) << 1);
-    btns[2] |= (buttonState(PIN_P2_B5));
-    btns[3] |= (buttonState(PIN_P2_B6) << 7);
-    btns[3] |= (buttonState(PIN_P2_B7) << 6);
+//    btns[0] |= (buttonState(PIN_P2_UP) << 3);
+//    btns[0] |= (buttonState(PIN_P2_DOWN) << 2);
+//    btns[0] |= (buttonState(PIN_P2_LEFT) << 1);
+//    btns[0] |= (buttonState(PIN_P2_RIGHT));
+//    btns[1] |= (buttonState(PIN_P1_COIN) << 7);
+//    btns[1] |= (buttonState(PIN_P1_START) << 6);
+//    btns[1] |= (buttonState(PIN_P1_B1) << 5);
+//    btns[1] |= (buttonState(PIN_P1_B2) << 4);
+//    btns[1] |= (buttonState(PIN_P1_B3) << 3);
+//    btns[1] |= (buttonState(PIN_P1_B4) << 2);
+//    btns[1] |= (buttonState(PIN_P1_B5) << 1);
+//    btns[1] |= (buttonState(PIN_P1_B6));
+//    btns[2] |= (buttonState(PIN_P1_B7) << 7);
+//    btns[2] |= (buttonState(PIN_P2_COIN) << 6);
+//    btns[2] |= (buttonState(PIN_P2_START) << 5);
+//    btns[2] |= (buttonState(PIN_P2_B1) << 4);
+//    btns[2] |= (buttonState(PIN_P2_B2) << 3);
+//    btns[2] |= (buttonState(PIN_P2_B3) << 2);
+//    btns[2] |= (buttonState(PIN_P2_B4) << 1);
+//    btns[2] |= (buttonState(PIN_P2_B5));
+//    btns[3] |= (buttonState(PIN_P2_B6) << 7);
+//    btns[3] |= (buttonState(PIN_P2_B7) << 6);
 
     Serial.write(btns, PAYLOAD_SIZE);
   }
@@ -154,4 +154,6 @@ int buttonState(int pin) {
   if(digitalRead(pin) == HIGH) {
     return 1;
   }
+
+  return 0;
 }
--- server/server.py
+++ server/server.py
@@ -34,13 +34,69 @@ import serial
 import time
 import win32api
 import win32con
+import ctypes
+
+# BLOCK: this block from http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# END BLOCK
 
 PAYLOAD_SIZE = 4
 
-VK_LEFT = 0x25
-VK_UP = 0x26
-VK_RIGHT = 0x27
-VK_DOWN = 0x28
+# DirectInput ScanCodes from http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+
+VK_LEFT = 0xCB
+VK_UP = 0xC8
+VK_RIGHT = 0xCD
+VK_DOWN = 0xD0
 VK_1 = 0x31
 VK_2 = 0x32
 VK_5 = 0x35
@@ -73,10 +129,12 @@ btns = bytearray([0,0,0,0])
 def update_button_state( byte_num, mask, vkey ):
 	if cached_btns[byte_num] & mask > btns[byte_num] & mask:
 		# button released, send KEYUP event
-		win32api.keybd_event(vkey, 0, win32con.KEYEVENTF_KEYUP, 0)
-	else if btns[byte_num] & mask > cached_btns[byte_num] & mask:
+		#win32api.keybd_event(vkey, 0, win32con.KEYEVENTF_KEYUP, 0)
+		PressKey(vkey)
+	elif btns[byte_num] & mask > cached_btns[byte_num] & mask:
 		# button pressed, send KEYDOWN event
-		win32api.keybd_event(vkey, 0, 0, 0)
+		#win32api.keybd_event(vkey, 0, 0, 0)
+		ReleaseKey(vkey)
 
 while True:
 	try:
@@ -85,48 +143,45 @@ while True:
 		# load button states
 		btns = bytearray([0,0,0,0])
 		for b in range(0,PAYLOAD_SIZE):
-			btns[b] = ss.read()
-			print('{0:08b}'.format(ord(val)) + ' ') # debugging: print binary representation of bytes
-		print('\n') # debugging: newline
+			btns[b] = ord(ss.read(1))
+			print('{0:08b}'.format(btns[b]), end=' ') # debugging: print binary representation of bytes
+		print()
 
 		# byte0
-		update_button_state(0, 0b10000000, VK_UP)
-		update_button_state(0, 0b01000000, VK_DOWN)
-		update_button_state(0, 0b00100000, VK_LEFT)
-		update_button_state(0, 0b00010000, VK_RIGHT)
-		update_button_state(0, 0b00001000, VK_W)
-		update_button_state(0, 0b00000100, VK_S)
-		update_button_state(0, 0b00000010, VK_A)
-		update_button_state(0, 0b00000001, VK_D)
+		update_button_state(1, 0b10000000, VK_UP)
+		update_button_state(1, 0b01000000, VK_DOWN)
+		update_button_state(1, 0b00100000, VK_LEFT)
+		update_button_state(1, 0b00010000, VK_RIGHT)
+		update_button_state(1, 0b00001000, VK_W)
+		update_button_state(1, 0b00000100, VK_S)
+		update_button_state(1, 0b00000010, VK_A)
+		update_button_state(1, 0b00000001, VK_D)
 		# byte1
-		update_button_state(0, 0b10000000, VK_5)
-		update_button_state(0, 0b01000000, VK_1)
-		update_button_state(0, 0b00100000, VK_L)
-		update_button_state(0, 0b00010000, VK_SEMICOLON)
-		update_button_state(0, 0b00001000, VK_QUOTE)
-		update_button_state(0, 0b00000100, VK_S)
-		update_button_state(0, 0b00000010, VK_A)
-		update_button_state(0, 0b00000001, VK_D)
-		# byte2
-		update_button_state(0, 0b10000000, VK_UP)
-		update_button_state(0, 0b01000000, VK_DOWN)
-		update_button_state(0, 0b00100000, VK_LEFT)
-		update_button_state(0, 0b00010000, VK_RIGHT)
-		update_button_state(0, 0b00001000, VK_W)
-		update_button_state(0, 0b00000100, VK_S)
-		update_button_state(0, 0b00000010, VK_A)
-		update_button_state(0, 0b00000001, VK_D)
+		update_button_state(2, 0b10000000, VK_5)
+		update_button_state(2, 0b01000000, VK_1)
+		update_button_state(2, 0b00100000, VK_L)
+		update_button_state(2, 0b00010000, VK_SEMICOLON)
+		update_button_state(2, 0b00001000, VK_QUOTE)
+		update_button_state(2, 0b00000100, VK_COMMA)
+		update_button_state(2, 0b00000010, VK_SLASH)
+		update_button_state(2, 0b00000001, VK_FULLSTOP)
+		# byte2mm62tttt
+		update_button_state(3, 0b10000000, VK_M)
+		update_button_state(3, 0b01000000, VK_6)
+		update_button_state(3, 0b00100000, VK_2)
+		update_button_state(3, 0b00010000, VK_T)
+		update_button_state(3, 0b00001000, VK_Y)
+		update_button_state(3, 0b00000100, VK_U)
+		update_button_state(3, 0b00000010, VK_G)
+		update_button_state(3, 0b00000001, VK_H)
 		# byte3
-		update_button_state(0, 0b10000000, VK_UP)
-		update_button_state(0, 0b01000000, VK_DOWN)
-
-		if ord(val) & 0b10000000 > 0:
-			win32api.keybd_event(f,0,0,0)
-			win32api.keybd_event(f,0,win32con.KEYEVENTF_KEYUP,0)
-		if ord(val) & 0b01000000 > 0:
-			win32api.keybd_event(g,0,0,0)
-			win32api.keybd_event(g,0,win32con.KEYEVENTF_KEYUP,0)
+		update_button_state(0, 0b10000000, VK_J)
+		update_button_state(0, 0b01000000, VK_F)
 
 	except serial.serialutil.SerialException:
 		release_keys()
 		ss.close()
+
+
+
+
